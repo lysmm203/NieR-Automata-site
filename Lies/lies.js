59d02adbcc232e61adbcf69b74043931eabac08e
Yong Seok Lee <lyeongsuk97@gmail.com>
2020-12-21 16:44:34 +0900

    Upload Files

---
+++ Lies/lies.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+// 	$(".navigate").click(function(){
+//    		$(".static").toggle();
+//         var button_array = $("a")
+//
+//         for (var i = 0; i < button_array.length; i++) {
+//
+//             button_array[i].style.display = "none";
+//         }
+//         setTimeout(function(){
+//             $(".static").toggle();
+//             $("a").toggle();
+//         }, 1000);
+//
+//
+//
+// 	});
+// });
+
+
+$("button").click(function(){
+    // var buttonHTML = this.innerHTML;
+    var song = new Audio("../kaine.mp3");
+    song.play();
+
+
+})
